681f7c5cb7eddacf73fb94e1707e74cfb114237e
Anu <anuradha.singh@aut.ac.nz>
2025-09-02 13:30:15 +1200

    Added Files

---
+++ src/teststudent/TestStudent.java
@@ -0,0 +1,23 @@
+package teststudent;
+
+/**
+ *
+ * @author anusingh
+ */
+public class TestStudent {
+
+     private String name;
+
+    public TestStudent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
